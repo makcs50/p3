a61c5b6a853a738d6ded16c09cc026290b653921
Abdullah <pal95@mail.ru>
2023-05-22 16:26:34 +0300

    p3

---
+++ index.js
@@ -0,0 +1,6 @@
+import os from 'os'
+import { pal } from './welcome.js'
+console.log(os.type())
+console.log(os.homedir())
+
+console.log(pal())
---
+++ welcome.js
@@ -0,0 +1,8 @@
+import os from 'os'
+
+export function pal() {
+    console.log(`Привет, юзер. Вижу ты зашел с${os.type()}`)
+}
+
+
+
